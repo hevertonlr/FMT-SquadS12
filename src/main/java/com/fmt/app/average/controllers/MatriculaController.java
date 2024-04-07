f8ee5ce4bfad69406fde6d4aac108910bab9226e
barbaracalderon <bcalderoni.ti@gmail.com>
2024-04-07 05:56:56 -0300

    update: MatriculaController

--- src/main/java/com/fmt/app/average/controllers/MatriculaController.java
+++ src/main/java/com/fmt/app/average/controllers/MatriculaController.java
@@ -1,14 +1,58 @@
 package com.fmt.app.average.controllers;
 
 import com.fmt.app.average.entities.MatriculaEntity;
-import com.fmt.app.average.interfaces.IGenericService;
 import com.fmt.app.average.services.MatriculaService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
-@RequestMapping("matriculas")
+@RequestMapping("/matriculas")
 public class MatriculaController {
+
+    @Autowired
+    private MatriculaService matriculaService;
+
+    @GetMapping
+    public ResponseEntity<List<MatriculaEntity>> listarMatriculas() {
+        List<MatriculaEntity> matriculas = matriculaService.listarTodasMatriculas();
+        return ResponseEntity.ok(matriculas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MatriculaEntity> buscarMatriculaPorId(@PathVariable Long id) {
+        MatriculaEntity matricula = matriculaService.buscarMatriculaPorId(id);
+        if (matricula != null) {
+            return ResponseEntity.ok(matricula);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<MatriculaEntity> cadastrarMatricula(@RequestBody MatriculaEntity matricula) {
+        MatriculaEntity novaMatricula = matriculaService.cadastrar(matricula);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaMatricula);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MatriculaEntity> atualizarMatricula(@PathVariable Long id, @RequestBody MatriculaEntity matricula) {
+        MatriculaEntity matriculaAtualizada = matriculaService.atualizar(id, matricula);
+        if (matriculaAtualizada != null) {
+            return ResponseEntity.ok(matriculaAtualizada);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarMatricula(@PathVariable Long id) {
+        matriculaService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
