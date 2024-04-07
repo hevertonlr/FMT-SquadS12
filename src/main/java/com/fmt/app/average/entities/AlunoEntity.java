c4e8d23caba8336ea7464800ef6d8ca8c18cd1f5
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-04-07 04:33:48 -0300

    Ajuste entity Aluno

--- src/main/java/com/fmt/app/average/entities/AlunoEntity.java
+++ src/main/java/com/fmt/app/average/entities/AlunoEntity.java
@@ -1,18 +1,39 @@
 package com.fmt.app.average.entities;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 
+@Data
 @Entity
 @Table(name = "aluno")
-@Data
+@RequiredArgsConstructor
 public class AlunoEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreationTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    protected LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    protected LocalDateTime modifyAt;
 
     @Column(length=150, nullable=false)
     private String nome;
