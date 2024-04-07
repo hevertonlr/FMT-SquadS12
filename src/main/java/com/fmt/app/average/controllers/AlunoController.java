ed04922fef98b70c8ebd10098d7c708089f05cf7
barbaracalderon <bcalderoni.ti@gmail.com>
2024-04-07 04:50:11 -0300

    update: mais métodos AlunoController

--- src/main/java/com/fmt/app/average/controllers/AlunoController.java
+++ src/main/java/com/fmt/app/average/controllers/AlunoController.java
@@ -3,10 +3,9 @@ package com.fmt.app.average.controllers;
 import com.fmt.app.average.entities.AlunoEntity;
 import com.fmt.app.average.services.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +20,38 @@ public class AlunoController {
     public ResponseEntity<List<AlunoEntity>> listarAlunos() {
         List<AlunoEntity> alunos = alunoService.listarTodosAlunos();
         return ResponseEntity.ok(alunos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AlunoEntity> buscarAlunoPorId(@PathVariable Long id) {
+        AlunoEntity aluno = alunoService.buscarAlunoPorId(id);
+        if (aluno != null) {
+            return ResponseEntity.ok(aluno);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<AlunoEntity> cadastrarAluno(@RequestBody AlunoEntity aluno) {
+        AlunoEntity novoAluno = alunoService.cadastrar(aluno);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoAluno);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AlunoEntity> atualizarAluno(@PathVariable Long id, @RequestBody AlunoEntity aluno) {
+        AlunoEntity alunoAtualizado = alunoService.atualizar(id, aluno);
+        if (alunoAtualizado != null) {
+            return ResponseEntity.ok(alunoAtualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarAluno(@PathVariable Long id) {
+        alunoService.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
