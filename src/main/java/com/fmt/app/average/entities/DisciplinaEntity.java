530f2f1a75f30b8f458ced949a0dc2935c3c8b48
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-04-07 04:19:07 -0300

    Ajuste entity Disciplina

--- src/main/java/com/fmt/app/average/entities/DisciplinaEntity.java
+++ src/main/java/com/fmt/app/average/entities/DisciplinaEntity.java
@@ -1,21 +1,46 @@
 package com.fmt.app.average.entities;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "disciplina")
-@EqualsAndHashCode(callSuper = true)
-public class DisciplinaEntity extends GenericEntity<DisciplinaEntity>{
+@RequiredArgsConstructor
+public class DisciplinaEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    protected LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    protected LocalDateTime modifyAt;
+
     @Column(length = 150,nullable = false)
     private String nome;
     @ManyToOne
     @JoinColumn(name = "professor_id",nullable = false)
     private ProfessorEntity professor;
 
-    @Override
     public void update(DisciplinaEntity source) {
         this.nome = source.getNome();
     }
