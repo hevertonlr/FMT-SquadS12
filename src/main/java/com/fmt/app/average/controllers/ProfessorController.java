fc491fc2e836b4d42f3d680ec2e46aa388818aba
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-04-07 05:08:32 -0300

    Ajuste Controller Professor

--- src/main/java/com/fmt/app/average/controllers/ProfessorController.java
+++ src/main/java/com/fmt/app/average/controllers/ProfessorController.java
@@ -23,55 +23,50 @@ public class ProfessorController {
 
     @GetMapping("{id}")
     public ResponseEntity<ProfessorEntity> findById(@PathVariable Long id) {
-        String requestedValue = (ServletUriComponentsBuilder.fromCurrentRequest()).buildAndExpand().getPath();
-        log.info("GET "+requestedValue+" -> Início");
+        log.info("GET Professores -> Início");
         ProfessorEntity entity = service.findById(id);
-        log.info("GET "+requestedValue+" -> Encontrado", id);
-        log.info("GET "+requestedValue+" -> 200 OK", id);
-        log.debug("GET "+requestedValue+" -> Response Body:\n{}\n", id, objetoParaJson(entity));
+        log.info("GET Professores -> Encontrado", id);
+        log.info("GET Professores -> 200 OK", id);
+        log.debug("GET Professores -> Response Body:\n{}\n", id, objetoParaJson(entity));
         return ResponseEntity.ok(entity);
     }
 
     @GetMapping
     public ResponseEntity<List<ProfessorEntity>> list() {
-        String requestedValue = (ServletUriComponentsBuilder.fromCurrentRequest()).buildAndExpand().getPath();
-        log.info("GET "+requestedValue+" -> Início");
+        log.info("GET Professores -> Início");
         List<ProfessorEntity> entities = service.findAll();
-        log.info("GET "+requestedValue+" -> Encontrados {} registros", entities.size());
-        log.info("GET "+requestedValue+" -> 200 OK");
-        log.debug("GET "+requestedValue+" -> Response Body:\n{}\n", objetoParaJson(entities));
+        log.info("GET Professores -> Encontrados {} registros", entities.size());
+        log.info("GET Professores -> 200 OK");
+        log.debug("GET Professores -> Response Body:\n{}\n", objetoParaJson(entities));
         return ResponseEntity.ok(entities);
     }
 
     @PostMapping
     public ResponseEntity<ProfessorEntity> create(@RequestBody ProfessorEntity entity) {
-        String requestedValue = (ServletUriComponentsBuilder.fromCurrentRequest()).buildAndExpand().getPath();
-        log.info("POST "+requestedValue);
+        log.info("POST Professores");
         entity = service.insert(entity);
-        log.info("POST "+requestedValue+" -> Cadastrado");
-        log.info("POST "+requestedValue+" -> 201 CREATED");
-        log.debug("POST "+requestedValue+" -> Response Body:\n{}\n", objetoParaJson(entity));
+        log.info("POST Professores -> Cadastrado");
+        log.info("POST Professores -> 201 CREATED");
+        log.debug("POST Professores -> Response Body:\n{}\n", objetoParaJson(entity));
         return ResponseEntity.status(HttpStatus.CREATED).body(entity); // Retorna 201
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ProfessorEntity> update(@RequestBody ProfessorEntity entity) {
-        String requestedValue = (ServletUriComponentsBuilder.fromCurrentRequest()).buildAndExpand().getPath();
-        log.info("PUT "+requestedValue, entity.getId());
+        log.info("PUT Professores", entity.getId());
         entity = service.update(entity);
-        log.info("PUT "+requestedValue+" -> Atualizado", entity.getId());
-        log.info("PUT "+requestedValue+" -> 200 OK", entity.getId());
-        log.debug("PUT "+requestedValue+" -> Response Body:\n{}\n", entity.getId(), objetoParaJson(entity));
+        log.info("PUT Professores -> Atualizado", entity.getId());
+        log.info("PUT Professores -> 200 OK", entity.getId());
+        log.debug("PUT Professores -> Response Body:\n{}\n", entity.getId(), objetoParaJson(entity));
         return ResponseEntity.ok(entity);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        String requestedValue = (ServletUriComponentsBuilder.fromCurrentRequest()).buildAndExpand().getPath();
-        log.info("DELETE "+requestedValue, id);
+        log.info("DELETE Professores", id);
         service.delete(id);
-        log.info("DELETE "+requestedValue+" -> Excluído", id);
-        log.info("DELETE "+requestedValue+" -> 204 NO CONTENT", id);
+        log.info("DELETE Professores -> Excluído", id);
+        log.info("DELETE Professores -> 204 NO CONTENT", id);
         return ResponseEntity.noContent().build(); // Retorna 204
     }
 }
