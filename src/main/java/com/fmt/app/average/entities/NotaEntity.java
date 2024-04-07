97d863528143b8f313b60984ee01b92dd0ab65c1
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-04-07 04:34:26 -0300

    Ajuste entity Nota

--- src/main/java/com/fmt/app/average/entities/NotaEntity.java
+++ src/main/java/com/fmt/app/average/entities/NotaEntity.java
@@ -1,19 +1,40 @@
 package com.fmt.app.average.entities;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "notas")
+@RequiredArgsConstructor
 public class NotaEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreationTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    protected LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    protected LocalDateTime modifyAt;
 
     @ColumnDefault(value = "0.00")
     @Column(precision = 5,scale = 2,nullable = false)
